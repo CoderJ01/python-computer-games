226d876acddde8985ddeb3f5c130914e6b00e095
Joshua <buildingguy@sbcglobal.net>
2022-06-25 14:40:33 -0700

    jokes

---
+++ 02_jokes/README.md
@@ -0,0 +1,4 @@
+# Jokes
+
+## Purpose
+For this program, the computer tells a joke. The user gives a response. Then the computer outputs the answer.
---
+++ 02_jokes/jokes.py
@@ -0,0 +1,38 @@
+# Jokes
+print('What do you get when yoy cross a snowman with a vampire?')
+input()
+print('Frostbite!')
+print() #Keep text from looking crowed with blank space
+print('What do dentists call an astronaut\'s cavity?') #\ indicates that succeeding character is a escape character
+input()
+print('A black hole')
+print()
+print('Knock knock!')
+input()
+print("Who's there?")
+input()
+print('Interruption cow.')
+input()
+print('Interupting cow wh', end='') # The blank is called a keyword argument. 'end' is a keywprd parameter. Use this to prevent the creation of a new line.
+print('-MOO!')
+
+#
+# additional concepts
+#
+
+# use double backslashes
+print('They flew away in a green\tela helicopter')
+print('vs.')
+print('They flew away in a green\\teal helicopter')
+
+# single and double quotes print the same string
+print('Hello World!')
+print("Hello World")
+
+# Don't mix quote types
+# print('Hello World") <-- produces syntax error
+
+# backslashes
+print('I asked to borrow Abe\'s car. He said "Sure."')
+print("She said, \" I can't believe you let them burrow the car.\"")
+
