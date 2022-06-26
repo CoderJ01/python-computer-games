fad9b76050152a90972287cd21e72260d13c4a22
Joshua <buildingguy@sbcglobal.net>
2022-06-25 19:39:22 -0700

    hangman design

---
+++ 05_Hangman-Flowchart/sample-run.py
@@ -0,0 +1,62 @@
+"""
+HANGMAN
+ +---+
+     |
+     |
+     |
+    ===
+Missed Letters:
+_ _ _
+
+# START
+Guess a letter.
+a
+ +---+
+     |
+     |
+     |
+    ===
+Missed Letters:
+_ a _
+
+# 2n Guess
+Guess a letter.
+o
+ +---+
+ 0   |
+     |
+     |
+    ===
+Missed letters: o
+_ a _
+
+# 3rd Guess
+Guess a letter.
+r
+ +---+
+ 0   |
+ |   |
+     |
+    ===
+Missed letters: or
+_ a _
+
+# 4th Guess
+Guess a letter.
+t
+ +---+
+ 0   |
+ |   |
+     |
+    ===
+_ a t
+
+# %th Guess
+Guess a letter
+a
+You have already guessed that letter. Chose again.
+c
+Yes! The wors is "cat"! You have won!
+Do you want to play agai? (yes or no)
+no
+"""
